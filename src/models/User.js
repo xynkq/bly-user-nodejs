64cb37f806d5de7c93a001bad353c2b977f14180
Seoyeon Kang <xynkq1@gmail.com>
2022-04-03 22:17:32 +0900

    [ADD] models : user

---
+++ src/models/User.js
@@ -0,0 +1,122 @@
+const { Op } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    idx: {
+      field: 'idx',
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      field: 'email',
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      primaryKey: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    mobile: {
+      field: 'mobile',
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
+    },
+    pw: {
+      field: 'pw',
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    name: {
+      field: 'name',
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    nickname: {
+      field: 'nickname',
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    created: {
+      field: 'created',
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated: {
+      field: 'updated',
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  }, {
+    tableName: 'user',
+    timestamps: false,
+  });
+
+  User.loginForEamil = (email, pw) => User.findOne({
+    attributes: ['idx', 'email', 'pw', 'name', 'nickname', 'mobile'],
+    where: {
+      email,
+      pw,
+    },
+    raw: true,
+  });
+
+  User.loginForMobile = (mobile, pw) => User.findOne({
+    attributes: ['idx', 'email', 'pw', 'name', 'nickname', 'mobile'],
+    where: {
+      mobile,
+      pw,
+    },
+    raw: true,
+  });
+
+  User.findAllForCheck = (email, mobile) => User.findOne({
+    attributes: ['idx', 'email', 'pw', 'name', 'nickname', 'mobile'],
+    where: {
+      [Op.or]: [
+        { email },
+        { mobile },
+      ]
+    },
+    raw: true,
+  });
+
+  User.changePassword = (idx, pw) => User.update({
+    pw,
+    updated: sequelize.literal('CURRENT_TIMESTAMP'),
+  }, {
+    where: {
+      idx,
+    },
+  });
+
+  User.findByEmail = (email) => User.findOne({
+    attributes: ['idx', 'email', 'pw', 'name', 'nickname', 'mobile'],
+    where: { email },
+    raw: true,
+  });
+
+  User.findByMobile = (mobile) => User.findOne({
+    attributes: ['idx', 'email', 'pw', 'name', 'nickname', 'mobile'],
+    where: { mobile },
+    raw: true,
+  });
+
+  User.findAllUsers = () => User.findAll({
+    attributes: ['idx', 'email', 'pw', 'name', 'nickname', 'mobile'],
+    order: [
+      ['idx', 'ASC'],
+    ],
+    nest: true,
+    raw: true,
+  });
+
+  User.removeUser = (idx) => User.destroy({
+    where: {
+      idx,
+    },
+  });
+
+  return User;
+};
---
+++ src/models/index.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const config = require('../../config/database');
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password, {
+    host: config.host,
+    dialect: config.dialect,
+    logging: true,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+    port: config.port,
+    timezone: 'Asia/Seoul',
+    dialectOptions: {
+			timezone: "local",
+		},
+  },
+);
+
+const models = {};
+
+fs.readdirSync(__dirname)
+  .filter(file => (file.indexOf('.') !== 0) && (file !== 'index.js'))
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    )
+    models[model.name] = model;
+  });
+
+Object.keys(models).forEach((modelName) => {
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+    console.log(modelName);
+  }
+});
+
+sequelize.sync().then(() => {
+  console.log('Schema is synchronized');
+}).catch((err) => {
+  console.log('An error has occurred: ', err);
+});
+
+console.log(models.User);
+
+models.sequelize = sequelize;
+models.Sequelize = sequelize;
+
+module.exports = models;
